d8afa65934d2313b528a273976b9220892cc57f5
Kvixen <Emil.wallentin@gmail.com>
2018-08-13 23:27:16 +0200

    Fixed a bug where pack.py would delete an existing zip

--- pack.py
+++ pack.py
@@ -3,5 +3,7 @@ from os import path, rename
 
 
 def main(dst, src, name):
+    if path.isfile(name + ".zip"):
+        name = name + "_fmu"
     shutil.make_archive(name, 'zip', src)
     rename(path.join(dst, name + '.zip'), name + ".fmu")
