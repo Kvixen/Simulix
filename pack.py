03340cbd9c2a92fb7bfe91890751bf840b071628
Emil Wallentin <Emil.wallentin@gmail.com>
2018-09-11 02:09:53 +0000

    Improved error handling of existing archive in build location

--- pack.py
+++ pack.py
@@ -1,9 +1,13 @@
-import shutil
+import shutil, uuid
 from os import path, rename
 
 
 def main(dst, src, name):
-    if path.isfile(name + ".zip"):
-        name = name + "_fmu"
-    shutil.make_archive(name, 'zip', src)
-    rename(path.join(dst, name + '.zip'), name + ".fmu")
+    if path.isfile(name + ".zip") or path.isfile(name + ".fmu"):
+        name += str(uuid.uuid4().hex)
+    try:
+        shutil.make_archive(name, 'zip', src)
+        rename(path.join(dst, name + '.zip'), name + ".fmu")
+    except FileExistsError:
+        exit("Archive %s already exists." % (name))
+        
